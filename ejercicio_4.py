240aabc3f95d97de9a93938c15cbb9827ffe8bcc
Maurojdiri <105528850+Maurojdiri@users.noreply.github.com>
2022-05-20 21:27:44 -0300

    Ejercicio N°4
    Completado.

---
+++ ejercicio_4.py
@@ -0,0 +1,29 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos variables de texto
+
+# Ingrese tres palabras y arme un acrónimo con ellas
+# Si desea puede modificar el código para ingresar más palabras
+print("ingrese palabra 1: united")
+palabra_1 = str(input())
+
+print('ingrese palabra 2: state')
+palabra_2 = str(input())
+
+print('ingrese palabra 3: america')
+palabra_3 = str(input())
+
+# De cada palabra debe tomar la primera letra y armar el acrónimo
+# Ejemplo: Alumbrado, barrido y limpieza --> ABL
+# Imprimir el resultado en pantalla
+
+print(palabra_1 [0], palabra_2[0], palabra_3[0])
+
+
